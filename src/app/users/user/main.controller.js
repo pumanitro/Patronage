ff80907c5a4b02c620cc62d99bc961ed8ae74180
pumanitro <pumanitro@gmail.com>
2016-01-07 19:46:40 +0100

    Created protection to User view for unexisting peoples.

--- src/app/users/user/main.controller.js
+++ src/app/users/user/main.controller.js
@@ -16,7 +16,13 @@
     vm.urlParams = $state.params;
 
     $http.get("https://api.github.com/users/"+vm.urlParams.login)
-       .then(function(response) {vm.user = response.data;});
+       .then(function(response) {
+            vm.user = response.data;
+        },function(response){
+            $("#userTop").hide();
+            $("#userContent").hide();
+            $("#userTopH2").html("<i class='fa fa-frown-o fa-5x'></i> This user doesn't exist.")
+        });
 
 
     activate();
