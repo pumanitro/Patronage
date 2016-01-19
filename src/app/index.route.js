7de405454e4cdb162a04c44f84cb04c9e71c0ccd
pumanitro <pumanitro@gmail.com>
2016-01-19 17:22:08 +0100

    Rechanged state providers

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,24 +13,20 @@
         templateUrl: 'app/home/home.html',
         controller: 'HomeController',
         controllerAs: 'HomeController'
-      });
-
-    $stateProvider
+      })
       .state('users', {
               url: '/users',
               templateUrl: 'app/users/users.html',
               controller: 'UsersController',
               controllerAs: 'UsersController'
-        });
-    $stateProvider
-          .state('about', {
+        })
+      .state('about', {
               url: '/about',
               templateUrl: 'app/about/about.html',
               controller: 'AboutController',
               controllerAs: 'AboutController'
-          });
-    $stateProvider
-          .state('user', {
+        })
+      .state('user', {
               url: '/users/:login',
               templateUrl: 'app/users/user/user.html',
               controller: 'UserController',
