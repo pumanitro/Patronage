355048dff5abbabb5a71fdbd44ed379b206decf3
pumanitro <pumanitro@gmail.com>
2016-01-20 18:36:10 +0100

    Deleted a lot of unnecessary code.

--- src/app/users/user/user.controller.js
+++ src/app/users/user/user.controller.js
@@ -8,11 +8,6 @@
   /** @ngInject */
   function UserController($timeout, webDevTec, toastr, $http, $state) {
     var vm = this;
-
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1451751192631;
-    vm.showToastr = showToastr;
     vm.urlParams = $state.params;
 
     $http.get("https://api.github.com/users/"+vm.urlParams.login)
@@ -21,31 +16,5 @@
         },function errorCallback(response) {
             $state.go('errors');
         });
-
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
-
-
-
   }
 })();
